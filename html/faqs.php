d58c56b759475b30978a6c40f64bf28c48f8e091
Piyawat.k <ravenza2012@gmail.com>
2024-08-12 18:57:07 +0700

    tdics-commit-09

--- html/faqs.php
+++ html/faqs.php
@@ -13,23 +13,52 @@
             <h1 style="text-align: center; margin: 0; font-size:8vw;">TDICS FAQs</h1>
         </header>
     </section>
-    <article style="margin: 1rem;">
-        <section class="section-history">
-            <article class="article-detail" style="display:flex; flex-direction: column; margin: 1rem;">
-                <h1 style="margin:0;font-size: 5vw;">Get In Touch</h1>
-                <p style="margin:0;font-size: 3vw;text-align:justify; text-justify: inter-word;">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-            </article>
+    <main>
+        <article style="margin: 1rem;">
+            <section class="section-history">
+                <article class="article-detail" style="display:flex; flex-direction: column; margin: 1rem;">
+                    <h1 style="margin:0;font-size: 5vw;">Top 5 FAQs</h1>
+                    <p style="margin:0;font-size: 3vw;text-align:justify; text-justify: inter-word;">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                </article>
+            </section>
+            <section style="margin: 0 1.5rem 0 1.5rem;">
+                <article style="margin-top: 1rem; padding:1rem;background-color:#D9D9D9;border-radius: 10px;">
+                    <h2 style="margin:0;font-size: 3vw;">TDICS?</h2>
+                    <p style="margin:0;font-size: 3vw;text-align:justify; text-justify: inter-word;">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                </article>
+                <article style="margin-top: 1rem; padding:1rem;background-color:#D9D9D9;border-radius: 10px;">
+                    <h2 style="margin:0;font-size: 3vw;">TDICS?</h2>
+                    <p style="margin:0;font-size: 3vw;text-align:justify; text-justify: inter-word;">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                </article>
+                <article style="margin-top: 1rem; padding:1rem;background-color:#D9D9D9;border-radius: 10px;">
+                    <h2 style="margin:0;font-size: 3vw;">TDICS?</h2>
+                    <p style="margin:0;font-size: 3vw;text-align:justify; text-justify: inter-word;">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                </article>
+                <article style="margin-top: 1rem; padding:1rem;background-color:#D9D9D9;border-radius: 10px;">
+                    <h2 style="margin:0;font-size: 3vw;">TDICS?</h2>
+                    <p style="margin:0;font-size: 3vw;text-align:justify; text-justify: inter-word;">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                </article>
+                <article style="margin-top: 1rem; padding:1rem;background-color:#D9D9D9;border-radius: 10px;">
+                    <h2 style="margin:0;font-size: 3vw;">TDICS?</h2>
+                    <p style="margin:0;font-size: 3vw;text-align:justify; text-justify: inter-word;">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                </article>
+            </section>
+            <hr style="margin:0.5rem; border:1.5px solid #F5F5F5;border-radius: 5px;">
+            <section class="section-history">
+                <article class="article-detail" style="display:flex; flex-direction: column; margin: 1rem;">
+                    <h1 style="margin:0;font-size: 5vw;">How We Can Help?</h1>
+                    <p style="margin:0;font-size: 3vw;text-align:justify; text-justify: inter-word;">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                </article>
+            </section>
+        </article>
+        <section>
+            <form id="contact-form" action="">
+                <section style="flex: 45%;width: 45%;margin: 0.5rem;">
+                    <input type="text" id="Name" name="Name">
+                </section>
+            </form>
         </section>
-    </article>
-    <hr style="margin:0.5rem; border:1.5px solid #F5F5F5;border-radius: 5px;">
-    <article style="margin: 1rem;">
-        <section class="section-history">
-            <article class="article-detail" style="display:flex; flex-direction: column; margin: 1rem;">
-                <h1 style="margin:0;font-size: 5vw;">Get In Touch</h1>
-                <p style="margin:0;font-size: 3vw;text-align:justify; text-justify: inter-word;">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-            </article>
-        </section>
-    </article>
+    </main>
 
     <?php include 'footer/footer.php'; ?>
 </body>
