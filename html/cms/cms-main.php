bae48cb586badd7d4da8dc9e906f1b98a72297ea
Piyawat Kanankaw <piyawat.k@tdicsouth.com>
2024-08-26 17:02:39 +0700

    tdicsouth-commit-10

---
+++ html/cms/cms-main.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en-us">
+
+<?php include '../header/header.php'; ?>
+
+<body>
+    <header>
+        <img id="index-header-logo" src="/Icon/tdics-logo.webp" alt="tdics logo" />
+        <img id="index-header-menu" src="/Icon/menu.webp" alt="tdics menu" />
+    </header>
+    <section style="display: flex; flex-direction:column;margin-top:12%;">
+        <header>
+            <h1 style="text-align:center; margin:0; font-size:60px;">CMS</h1>
+            <h2 style="text-align: center; margin:0; font-size:20px;">TDICSOUTH</h2>
+        </header>
+    </section>
+    <main style="display: flex; flex-wrap:wrap;margin:1rem;justify-content:center;">
+        <section class="cms-manu-flex">
+            <img id="cms-manu-img" src="/Icon/News.webp" alt="Menu-News">
+            <h1>News</h1>
+        </section>
+        <section class="cms-manu-flex">
+            <img id="cms-manu-img" src="/Icon/knowledge.webp" alt="Menu-News">
+            <h1>Knowledge</h1>
+        </section>
+        <section class="cms-manu-flex">
+            <img id="cms-manu-img" src="/Icon/Box.webp" alt="Menu-News">
+            <h1>Products</h1>
+        </section>
+        <section class="cms-manu-flex">
+            <img id="cms-manu-img" src="/Icon/Hiring.webp" alt="Menu-News">
+            <h1>Jobs</h1>
+        </section>
+        <section class="cms-manu-flex">
+            <img id="cms-manu-img" src="/Icon/Chat.webp" alt="Menu-News">
+            <h1>Message</h1>
+        </section>
+        <section class="cms-manu-flex">
+            <img id="cms-manu-img" src="/Icon/Logout.webp" alt="Menu-News">
+            <h1>Logout</h1>
+        </section>
+    </main>
+    <section>
+        <article style="margin: 1.5rem;">
+            <hr style="border: 1.5px solid #F5F5F5;border-radius: 5px;">
+            <h5 style="text-align: center;font-size:3vw;">Copyright @ 2024 TDICSouth.com all rights reserved</h5>
+            <hr style="border: 1.5px solid #ffffff;">
+        </article>
+    </section>
+</body>
+
+</html>
---
+++ html/cms/cms-news.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en-us">
+
+<?php include '../header/header.php'; ?>
+
+<body>
+    <header>
+        <img id="index-header-logo" src="/Icon/tdics-logo.webp" alt="tdics logo" />
+        <img id="index-header-menu" src="/Icon/menu.webp" alt="tdics menu" />
+    </header>
+    <section style="display: flex; flex-direction:column;margin-top:12%;">
+        <header>
+            <h1 style="text-align:center; margin:0; font-size:60px;">CMS</h1>
+            <h2 style="text-align: center; margin:0; font-size:20px;">TDICSOUTH</h2>
+        </header>
+    </section>
+    <main style="display: flex; flex-wrap:wrap;margin:1rem;justify-content:center;">
+        <section>
+            <img id="cms-news-img" src="/Icon/News.webp" alt="Menu-News">
+        </section>
+    </main>
+    <section>
+        <article style="margin: 1.5rem;">
+            <hr style="border: 1.5px solid #F5F5F5;border-radius: 5px;">
+            <h5 style="text-align: center;font-size:3vw;">Copyright @ 2024 TDICSouth.com all rights reserved</h5>
+            <hr style="border: 1.5px solid #ffffff;">
+        </article>
+    </section>
+</body>
+
+</html>
